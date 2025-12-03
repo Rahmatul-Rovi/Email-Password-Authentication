0fa5bfab75741053e959b086b578c1c950e2bab8
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2025-12-04 00:05:39 +0600

    register section added

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import Root from './Layout/Root.jsx'
 import Home from './Components/Home/Home.jsx'
+import Login from './Components/Login/Login.jsx'
+import Register from './Components/Register/Register.jsx'
 
 const router = createBrowserRouter([
   {
@@ -12,7 +14,16 @@ const router = createBrowserRouter([
   Component: Root,
   children: [{
     index: true, Component: Home
-  }]
+  },
+  {
+    path: 'login',
+    Component: Login
+  },
+  {
+    path: 'register',
+    Component: Register
+  }
+]
 }])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
