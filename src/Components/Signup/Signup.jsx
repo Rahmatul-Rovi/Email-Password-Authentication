c074f1c21b48371a896692c151c1fe9f5fe1201d
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2025-12-05 00:08:24 +0600

    toggle password

--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -1,57 +1,85 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { auth } from "../../Firebase.init";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Signup = () => {
   const [success, setSuccess] = useState(false);
-     const [errorMessage, setErrorMessage] = useState('');
-    const handleSignUp = e => {
-       
-        e.preventDefault();
-        const email = e.target.email.value;
-        const password = e.target.password.value;
-        console.log(email, password);
-         setSuccess(false);
-        setErrorMessage('');
-
-      //Password validation 
-      const passwordRegEx = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
-      if(passwordRegEx.test(password) === false){
-      setErrorMessage('Password should be at least 8 characters, at least one uppercase letter, at least one lowercase letter, and at least one number');
-      
-      }
-
-        // Create User
-        createUserWithEmailAndPassword(auth, email, password)
-        .then(result => {
-            console.log(result);
-            console.log(true);
-        })
-        .catch(error => {
-            console.log(error);
-            setErrorMessage(error.message);
-        })
+  const [errorMessage, setErrorMessage] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const terms = e.target.terms.checked;
+    console.log(email, password, terms);
+    setSuccess(false);
+    setErrorMessage("");
+    if(!terms){
+      setErrorMessage('You have to accept terms and conditions');
+      return;
     }
+
+    //Password validation
+    const passwordRegEx = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
+    if (passwordRegEx.test(password) === false) {
+      setErrorMessage(
+        "Password should be at least 8 characters, at least one uppercase letter, at least one lowercase letter, and at least one number"
+      );
+    }
+
+    // Create User
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result);
+        console.log(true);
+      })
+      .catch((error) => {
+        console.log(error);
+        setErrorMessage(error.message);
+      });
+  };
   return (
     <div className="card bg-base-100 mx-auto w-full max-w-sm shrink-0 shadow-2xl">
       <div className="card-body">
         <h1 className="text-3xl font-bold">Please Sign Up now!</h1>
-        <form onSubmit={handleSignUp} >
+        <form onSubmit={handleSignUp}>
           <label className="label">Email</label>
-          <input type="email" name="email" className="input" placeholder="Email" />
+          <input
+            type="email"
+            name="email"
+            className="input"
+            placeholder="Email"
+          />
           <label className="label mt-4">Password</label>
-          <input type="password" name="password" className="input" placeholder="Password" />
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              name="password"
+              className="input"
+              placeholder="Password"
+            />
+            <button
+              onClick={() => {
+                setShowPassword(!showPassword);
+              }}
+              className="btn btn-xs absolute top-2 right-6"
+            >
+              {showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
+            </button>
+          </div>
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>
+          <label className="label mt-2">
+            <input type="checkbox" name="terms" className="checkbox" />
+            Accept terms & conditions
+          </label>
+          <br />
           <button className="btn btn-neutral mt-4">Sign Up</button>
         </form>
-        {
-            errorMessage && <p className="text-red-500">{errorMessage}</p>
-        }
-        {
-          success && <p className="text-green-500">User created Successfully</p>
-        }
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        {success && <p className="text-green-500">User created Successfully</p>}
       </div>
     </div>
   );
